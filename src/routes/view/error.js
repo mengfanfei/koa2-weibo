75a654ec29d0be61ab88e72d2dde0ca56b6f2107
mengfanfei <mengff0924@163.com>
2020-07-18 01:06:58 +0800

    修改缩进

--- src/routes/view/error.js
+++ src/routes/view/error.js
@@ -6,11 +6,11 @@
 const router = require('koa-router')()
 
 router.get('/error', async (ctx, next) => {
-    await ctx.render('error')
+  await ctx.render('error')
 })
 
 router.get('*', async (ctx, next) => {
-    await ctx.render('404')
+  await ctx.render('404')
 })
 
 module.exports = router
